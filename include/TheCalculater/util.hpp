3ca8aa1171a7080fde2b910624463c7aea308924
prbegd <1225095986@qq.com>
2025-07-07 21:25:48 +0800

    feat: add throw_with_trace

---
+++ include/TheCalculater/util.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file util.hpp
+ * @author prbegd
+ * @brief Utility functions and types.
+ * @date 2025-06-21
+ *
+ * Copyright © 2025 prbegd & TheCalculater contributors
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *
+ */
+#pragma once
+#include <boost/exception/all.hpp>
+#include <boost/stacktrace.hpp>
+
+typedef boost::error_info<struct tag_stacktrace, boost::stacktrace::stacktrace> traced;
+template <class E>
+void throw_with_trace(const E& e) {
+    throw boost::enable_error_info(e)
+        << traced(boost::stacktrace::stacktrace());
+}
