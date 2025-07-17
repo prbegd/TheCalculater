e6a4cdf0faed476626422a4db41b94f8698adfcb
prbegd <1225095986@qq.com>
2025-07-17 14:01:30 +0800

    feat: update ts file

--- translations/TheCalculater_zh_CN.ts
+++ translations/TheCalculater_zh_CN.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/main.cpp" line="92"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>复制</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../subprojects/CrashHandler/crash_handler_dialog.ui" line="23"/>
         <source>TheCalculater Encountered a Problem</source>
-        <translation type="unfinished"></translation>
+        <translation>TheCalculater遇到了问题</translation>
     </message>
     <message>
         <location filename="../subprojects/CrashHandler/crash_handler_dialog.ui" line="50"/>
@@ -30,27 +30,38 @@ You can try to restart the program. If the problem persists, please report this 
 5. Finally, *describing the steps* you took when encountering the crash will be very helpful to us!
 
 Thanks for your understanding and support! Your feedback helps make TheCalculater more stable and reliable.</source>
-        <translation type="unfinished"></translation>
+        <translation>## 哎呀！TheCalculater 遇到了问题且需要关闭。
+
+您可以尝试重启程序。如果问题仍然存在，请按照**以下步骤**报告此问题：
+
+1. 访问我们的[Github Issues 页面](https://github.com/prbegd/TheCalculater/issues)以[报告此问题](https://github.com/prbegd/TheCalculater/issues/new)。
+   https://github.com/prbegd/TheCalculater/issues
+2. 崩溃报告已经保存至 &apos;%1&apos;。点击下方的&quot;打开崩溃报告&quot;按钮来打开它。
+3. 报告问题时请**务必附上**上述提到的崩溃报告文件。
+4. 您也可以*附上日志文件*（点击下面的“打开日志文件”按钮来打开）来帮助我们更好地解决此问题。
+5. 最后，*描述您遇到崩溃时的步骤*会对我们很有帮助！
+
+感谢您的理解与支持！您的反馈会让 TheCalculater 更稳定可靠。</translation>
     </message>
     <message>
         <location filename="../subprojects/CrashHandler/crash_handler_dialog.ui" line="119"/>
         <source>Open Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>打开崩溃报告</translation>
     </message>
     <message>
         <location filename="../subprojects/CrashHandler/crash_handler_dialog.ui" line="129"/>
         <source>Open Log File</source>
-        <translation type="unfinished"></translation>
+        <translation>打开日志文件</translation>
     </message>
     <message>
         <location filename="../subprojects/CrashHandler/crash_handler_dialog.ui" line="139"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭</translation>
     </message>
     <message>
         <location filename="../subprojects/CrashHandler/crash_handler_dialog.ui" line="146"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>重启</translation>
     </message>
 </context>
 <context>
@@ -58,13 +69,14 @@ Thanks for your understanding and support! Your feedback helps make TheCalculate
     <message>
         <location filename="../src/main.cpp" line="170"/>
         <source>Failed to load resource file</source>
-        <translation type="unfinished"></translation>
+        <translation>加载资源文件失败</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="170"/>
         <source>Unable to load resource file, program startup failed!
 The resources.rcc in the program directory may have been deleted or damaged. You can try reinstalling the program to solve this problem.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法加载资源文件，程序启动失败！
+程序目录下的resources.rcc文件可能被删除或已损坏。您可以尝试重新下载程序来解决此问题。</translation>
     </message>
 </context>
 <context>
