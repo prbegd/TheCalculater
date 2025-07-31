19066c2cc58eef33bcc8c4f4f8923f448d5554bf
prbegd <1225095986@qq.com>
2025-07-31 15:24:19 +0800

    feat: add StringValue

---
+++ include/TheCalculater/settings.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file settings.hpp
+ * @author prbegd
+ * @brief Provides settings for TheCalculater.
+ * @date 2025-07-30
+ *
+ * Copyright © 2025 prbegd & TheCalculater contributors
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *
+ */
+
+#include <QString>
+#include <utility> // for std::move
+
+namespace TheeCalculater::settings {
+    /**
+     * @brief A class representing a string value in settings.
+     */
+    class StringValue {
+    private:
+        const std::string value;
+
+    public:
+        StringValue(std::string val)
+            : value(std::move(val))
+        { }
+
+        [[nodiscard]] std::string string() const { return value; }
+        [[nodiscard]] QString qString() const { return QString::fromStdString(value); }
+
+        operator std::string() const { return value; }
+        operator QString() const { return QString::fromStdString(value); }
+    };
+} // namespace TheeCalculater::settings
