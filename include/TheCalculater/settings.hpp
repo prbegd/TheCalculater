340495dd7369481de827803fccd83c5b9dbb33de
prbegd <1225095986@qq.com>
2025-08-02 17:16:15 +0800

    feat: add IntegerValue and DecimalValue

--- include/TheCalculater/settings.hpp
+++ include/TheCalculater/settings.hpp
@@ -9,7 +9,10 @@
  *
  */
 
+#include "TheCalculater/math/detail.hpp"
+#include "boost/multiprecision/fwd.hpp"
 #include <QString>
+#include <cstdint>
 #include <utility>
 
 namespace TheeCalculater::settings {
@@ -30,5 +33,86 @@ namespace TheeCalculater::settings {
 
         operator std::string() const { return value; }
         operator QString() const { return QString::fromStdString(value); }
+    };
+
+    class IntegerValue {
+    private:
+        boost::multiprecision::cpp_int value;
+
+    public:
+        IntegerValue(boost::multiprecision::cpp_int val)
+            : value(std::move(val))
+        { }
+
+        [[nodiscard]] boost::multiprecision::cpp_int cpp_int() const { return value; }
+        [[nodiscard]] TheCalculater::math::_fraction fraction() const { return value; }
+        [[nodiscard]] int8_t int8() const { return value.convert_to<int8_t>(); }
+        [[nodiscard]] int16_t int16() const { return value.convert_to<int16_t>(); }
+        [[nodiscard]] int32_t int32() const { return value.convert_to<int32_t>(); }
+        [[nodiscard]] int64_t int64() const { return value.convert_to<int64_t>(); }
+        [[nodiscard]] uint8_t uint8() const { return value.convert_to<uint8_t>(); }
+        [[nodiscard]] uint16_t uint16() const { return value.convert_to<uint16_t>(); }
+        [[nodiscard]] uint32_t uint32() const { return value.convert_to<uint32_t>(); }
+        [[nodiscard]] uint64_t uint64() const { return value.convert_to<uint64_t>(); }
+
+        [[nodiscard]] float fp32() const { return value.convert_to<float>(); }
+        [[nodiscard]] double fp64() const { return value.convert_to<double>(); }
+        /// @warning The bit-size of long double is platform dependent.
+        [[nodiscard]] long double fp128() const { return value.convert_to<long double>(); }
+
+        operator boost::multiprecision::cpp_int() const { return value; }
+        operator TheCalculater::math::_fraction() const { return value; }
+        operator int8_t() const { return value.convert_to<int8_t>(); }
+        operator int16_t() const { return value.convert_to<int16_t>(); }
+        operator int32_t() const { return value.convert_to<int32_t>(); }
+        operator int64_t() const { return value.convert_to<int64_t>(); }
+        operator uint8_t() const { return value.convert_to<uint8_t>(); }
+        operator uint16_t() const { return value.convert_to<uint16_t>(); }
+        operator uint32_t() const { return value.convert_to<uint32_t>(); }
+        operator uint64_t() const { return value.convert_to<uint64_t>(); }
+
+        operator float() const { return value.convert_to<float>(); }
+        operator double() const { return value.convert_to<double>(); }
+        operator long double() const { return value.convert_to<long double>(); }
+    };
+
+    class DecimalValue {
+    private:
+        TheCalculater::math::_fraction value;
+
+    public:
+        DecimalValue(TheCalculater::math::_fraction val)
+            : value(std::move(val))
+        { }
+        [[nodiscard]] TheCalculater::math::_fraction fraction() const { return value; }
+        [[nodiscard]] float fp32() const { return value.numerator().convert_to<float>() / value.denominator().convert_to<float>(); }
+        [[nodiscard]] double fp64() const { return value.numerator().convert_to<double>() / value.denominator().convert_to<double>(); }
+        /// @warning The bit-size of long double is platform dependent.
+        [[nodiscard]] long double fp128() const { return value.numerator().convert_to<long double>() / value.denominator().convert_to<long double>(); }
+
+        [[nodiscard]] boost::multiprecision::cpp_int cpp_int() const { return value.numerator() / value.denominator(); }
+        [[nodiscard]] int8_t int8() const { return static_cast<int8_t>(value.numerator().convert_to<int32_t>() / value.denominator().convert_to<int32_t>()); }
+        [[nodiscard]] int16_t int16() const { return static_cast<int16_t>(value.numerator().convert_to<int32_t>() / value.denominator().convert_to<int32_t>()); }
+        [[nodiscard]] int32_t int32() const { return value.numerator().convert_to<int32_t>() / value.denominator().convert_to<int32_t>(); }
+        [[nodiscard]] int64_t int64() const { return value.numerator().convert_to<int64_t>() / value.denominator().convert_to<int64_t>(); }
+        [[nodiscard]] uint8_t uint8() const { return value.numerator().convert_to<uint8_t>() / value.denominator().convert_to<uint8_t>(); }
+        [[nodiscard]] uint16_t uint16() const { return value.numerator().convert_to<uint16_t>() / value.denominator().convert_to<uint16_t>(); }
+        [[nodiscard]] uint32_t uint32() const { return value.numerator().convert_to<uint32_t>() / value.denominator().convert_to<uint32_t>(); }
+        [[nodiscard]] uint64_t uint64() const { return value.numerator().convert_to<uint64_t>() / value.denominator().convert_to<uint64_t>(); }
+
+        operator TheCalculater::math::_fraction() const { return value; }
+        operator float() const { return value.numerator().convert_to<float>() / value.denominator().convert_to<float>(); }
+        operator double() const { return value.numerator().convert_to<double>() / value.denominator().convert_to<double>(); }
+        operator long double() const { return value.numerator().convert_to<long double>() / value.denominator().convert_to<long double>(); }
+
+        operator boost::multiprecision::cpp_int() const { return value.numerator() / value.denominator(); }
+        operator int8_t() const { return static_cast<int8_t>(value.numerator().convert_to<int32_t>() / value.denominator().convert_to<int32_t>()); }
+        operator int16_t() const { return static_cast<int16_t>(value.numerator().convert_to<int32_t>() / value.denominator().convert_to<int32_t>()); }
+        operator int32_t() const { return value.numerator().convert_to<int32_t>() / value.denominator().convert_to<int32_t>(); }
+        operator int64_t() const { return value.numerator().convert_to<int64_t>() / value.denominator().convert_to<int64_t>(); }
+        operator uint8_t() const { return value.numerator().convert_to<uint8_t>() / value.denominator().convert_to<uint8_t>(); }
+        operator uint16_t() const { return value.numerator().convert_to<uint16_t>() / value.denominator().convert_to<uint16_t>(); }
+        operator uint32_t() const { return value.numerator().convert_to<uint32_t>() / value.denominator().convert_to<uint32_t>(); }
+        operator uint64_t() const { return value.numerator().convert_to<uint64_t>() / value.denominator().convert_to<uint64_t>(); }
     };
 } // namespace TheeCalculater::settings
