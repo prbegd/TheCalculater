47c65e302760c71123cd1fee133784d720ef47ca
prbegd <1225095986@qq.com>
2025-09-06 17:25:22 +0800

    refactor: change return ""; to return {};

--- src/TheCalculater/dbgutil.cpp
+++ src/TheCalculater/dbgutil.cpp
@@ -131,7 +131,7 @@ namespace TheCalculater::dbgutil {
         {
             auto exception = std::current_exception();
             if (!exception)
-                return "";
+                return {};
             try {
                 std::rethrow_exception(exception);
             } catch (const std::exception& e) {
