e8b6d2762dd4a2714a3d16e67c85c13aaaf25e11
prbegd <1225095986@qq.com>
2025-11-02 15:41:03 +0800

    chore: fix artifact has no plugin

--- scripts/linuxcopylib.sh
+++ scripts/linuxcopylib.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-# linuxcopylib.sh
+# linuxcopylib.sh to copy all the dependencies of TheCalculater to a directory
 
-TARGET=$1
-DEST_DIR=$2
+TARGET="TheCalculater"
+DEST_DIR="."
+QT_ARCH=linux_gcc_64
+QT_PLUGIN_DIR="$QT_ROOT_DIR/plugins"
+
+# NOTE: Manually add the plugins you need
+QT_PLUGINS=(
+    "platforms/libqxcb.so"
+    "platforms/libqwayland.so"
+    "imageformats/libqico.so"
+    "imageformats/libqsvg.so"
+    "imageformats/libqjpeg.so"
+)
 
 LIB_BLACKLIST=(
     "^/lib"
@@ -13,19 +24,30 @@ LIB_BLACKLIST=(
     "libTheCalculaterCommon.so"
 )
 
-ldd "$TARGET" | grep "=> /" | awk '{print $3}' | while read -r lib; do
-    is_in_blacklist=0
-    for lib_blacklist in "${LIB_BLACKLIST[@]}"; do
-        if [[ "$lib" =~ $lib_blacklist ]]; then
-            is_in_blacklist=1
-            break
+copylibfor() {
+    ldd "$1" | grep "=> /" | awk '{print $3}' | while read -r lib; do
+        is_in_blacklist=0
+        for lib_blacklist in "${LIB_BLACKLIST[@]}"; do
+            if [[ "$lib" =~ $lib_blacklist ]]; then
+                is_in_blacklist=1
+                break
+            fi
+        done
+        
+        if [[ $is_in_blacklist -eq 0 && -f "$lib" ]]; then
+            echo "Copying library: $lib for $1"
+            cp "$lib" "$DEST_DIR"
         fi
     done
-    
-    if [[ $is_in_blacklist -eq 0 && -f "$lib" ]]; then
-        echo "Copying: $lib"
-        cp -v "$lib" "$DEST_DIR"
-    fi
+}
+
+copylibfor $TARGET
+
+for plugin in "${QT_PLUGINS[@]}"; do
+    pluginPath="$QT_PLUGIN_DIR/$plugin"
+    echo "Copying plugin: $plugin for $TARGET"
+    cp "$pluginPath" "$DEST_DIR/$plugin"
+    copylibfor "$pluginPath"
 done
 
-echo "Copied libraries to $DEST_DIR"
+echo "Dependencies copied to $DEST_DIR"
