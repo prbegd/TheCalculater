3cd7204e32aaa2a01e8cdfa6f5c813536e2b6f63
prbegd <1225095986@qq.com>
2025-11-02 18:34:00 +0800

    fix: huh issue

--- src/common/core.cpp
+++ src/common/core.cpp
@@ -16,6 +16,9 @@
 namespace TheCalculater::core {
     void registerLogger(const std::shared_ptr<spdlog::logger>& logger)
     {
+        // logger registered in main program weirdly synced to here (in linux ci build)
+        // Just for safe, we check if the logger is already registered.
+        if (spdlog::get(logger->name())) return;
         spdlog::register_logger(logger);
         spdlog::set_default_logger(logger);
     }
