263437875b4c1894f8df43f8c319154f5ee58a92
prbegd <1225095986@qq.com>
2025-12-21 18:00:27 +0800

    test: temp test solution

---
+++ test/test_main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file test_main.cpp
+ * @author prbegd
+ * @date 2025-12-21
+ *
+ * Copyright © 2025 Cai Yaoxing
+ * SPDX-License-Identifier: GPL-3.0-only
+ * This file is part of TheCalculater.
+ * See the file LICENSE in the project root or go to
+ * <https://www.gnu.org/licenses/gpl-3.0.html> for detailed license information.
+ */
+// TODO: Use catch2 framework to replace this (temporary solution)
+#define TEST_CASE(name) std::cout << "\n  Test case: " << name << '\n';
+#define REQUIRE(expr)                                     \
+    if (!(expr)) {                                        \
+        std::cout << "Require failed: " << #expr << '\n'; \
+        return 1;                                         \
+    }
+#define CHECK(expr)                                     \
+    if (!(expr)) {                                      \
+        std::cout << "Check failed: " << #expr << '\n'; \
+    }
+
+#include <iostream>
+
+int main()
+{
+}
