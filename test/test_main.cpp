e47bb3a55cbdfd5b52ccf03a9dc3b66f17a0044f
prbegd <90972353+prbegd@users.noreply.github.com>
2026-04-02 23:24:36 +0800

    attempt 33

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -10,6 +10,7 @@
  * <https://www.gnu.org/licenses/gpl-3.0.html> for detailed license information.
  */
 // TODO: Use catch2 v3 to replace this old-fashioned temporary solution.
+import std.compat;
 #define CATCH_CONFIG_RUNNER
 #include "catch2/catch.hpp" // IWYU pragma: keep
 
