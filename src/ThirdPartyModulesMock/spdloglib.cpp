5740d0eb45e18bf9004de6300448c961a55e12df
prbegd <90972353+prbegd@users.noreply.github.com>
2026-05-04 12:40:45 +0800

    package manger conan integration [dbglinux]

--- src/ThirdPartyModulesMock/spdloglib.cpp
+++ src/ThirdPartyModulesMock/spdloglib.cpp
@@ -31,7 +31,7 @@
 //
 // color sinks
 //
-// #ifdef _WIN32
+#ifdef _WIN32
 #include <spdlog/sinks/wincolor_sink-inl.h>
 template class SPDLOG_API spdlog::sinks::wincolor_sink<spdlog::details::console_mutex>;
 template class SPDLOG_API spdlog::sinks::wincolor_sink<spdlog::details::console_nullmutex>;
@@ -39,6 +39,7 @@ template class SPDLOG_API spdlog::sinks::wincolor_stdout_sink<spdlog::details::c
 template class SPDLOG_API spdlog::sinks::wincolor_stdout_sink<spdlog::details::console_nullmutex>;
 template class SPDLOG_API spdlog::sinks::wincolor_stderr_sink<spdlog::details::console_mutex>;
 template class SPDLOG_API spdlog::sinks::wincolor_stderr_sink<spdlog::details::console_nullmutex>;
+#endif
 // #else
 #include <spdlog/sinks/ansicolor_sink-inl.h>
 template class SPDLOG_API spdlog::sinks::ansicolor_sink<spdlog::details::console_mutex>;
