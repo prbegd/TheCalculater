dc06980e8add11ed36e8dba6d82e7c999da54ee9
prbegd <90972353+prbegd@users.noreply.github.com>
2026-08-28 22:22:18 +0800

    fix: rules

--- src/TheCalculaterCore/prbegd/thecalculater/math-analytic_expression.cpp
+++ src/TheCalculaterCore/prbegd/thecalculater/math-analytic_expression.cpp
@@ -1390,27 +1390,35 @@ AnalyticExpression::Simplification::generateDefaultRules(std::pmr::memory_resour
             return std::ranges::all_of(map.at('A'), [](const auto& node) { return typeid(*node) == typeid(Constant); });
         },
         [](auto map, const auto& context) {
-            auto constantPivot =
-                std::ranges::partition(map.at('A'), [](const auto& node) { return typeid(*node) == typeid(Constant); }).begin();
-            static_cast<Constant&>(*map.at('A').front()).value = std::ranges::fold_left(map.at('A').begin(), constantPivot, Rational(), [](const auto& accumulation, const auto& node) -> Rational {
-                return accumulation + static_cast<const Constant&>(*node).value;
-            });
+            auto constantPivot = std::ranges::partition(map.at('A'), [](const auto& node) {
+                                     return typeid(*node) == typeid(Constant);
+                                 }).begin();
+            static_cast<Constant&>(*map.at('A').front()).value =
+                std::ranges::fold_left(map.at('A').begin(),
+                                       constantPivot,
+                                       Rational(),
+                                       [](const auto& accumulation, const auto& node) -> Rational {
+                                           return accumulation + static_cast<const Constant&>(*node).value;
+                                       });
             map.at('A').erase(map.at('A').begin() + 1, constantPivot);
-            return util::makeUniquePmr<Addition>(context.memoryResource);
+            return util::makeUniquePmr<Addition>(context.memoryResource, std::move(map.at('A')));
         });
     rules.emplace_back(
-        util::makeUniquePmr<Multiplication>(memoryResource, util::makeUniquePmr<Wildcard::Variadic>(memoryResource, 'A')),
+        util::makeUniquePmr<Multiplication>(memoryResource,
+                                            util::makeUniquePmr<Wildcard::Variadic>(memoryResource, 'A')),
         [](const auto&, const auto& map, const auto&) {
             return std::ranges::all_of(map.at('A'), [](const auto& node) { return typeid(*node) == typeid(Constant); });
         },
         [](auto map, const auto& context) {
-            auto constantPivot =
-                std::ranges::partition(map.at('A'), [](const auto& node) { return typeid(*node) == typeid(Constant); }).begin();
-            static_cast<Constant&>(*map.at('A').front()).value = std::ranges::fold_left(map.at('A').begin(), constantPivot, Rational(1), [](const auto& product, const auto& node) -> Rational {
-                return product * static_cast<const Constant&>(*node).value;
-            });
+            auto constantPivot = std::ranges::partition(map.at('A'), [](const auto& node) {
+                                     return typeid(*node) == typeid(Constant);
+                                 }).begin();
+            static_cast<Constant&>(*map.at('A').front()).value = std::ranges::fold_left(
+                map.at('A').begin(), constantPivot, Rational(1), [](const auto& product, const auto& node) -> Rational {
+                    return product * static_cast<const Constant&>(*node).value;
+                });
             map.at('A').erase(map.at('A').begin() + 1, constantPivot);
-            return util::makeUniquePmr<Multiplication>(context.memoryResource);
+            return util::makeUniquePmr<Multiplication>(context.memoryResource, std::move(map.at('A')));
         });
     return rules;
 }
